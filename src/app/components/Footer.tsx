58498c5bbdd96dcc558cc60c4b02cabbe72b86d8
ipediez <ipediez93@gmail.com>
2025-05-29 15:57:35 +0200

    Updated sitemap and fixed GitHub link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,8 +4,8 @@ import SocialIcons from "./SocialIcons";
 const Footer = () => {
   return (
     <footer className="bg-black text-white p-6 flex justify-between items-center" translate="no">
-      <Link href="https://github.com/irene" className="text-sm text-gray-400">
-        code by Irene
+      <Link href="https://github.com/ipediez" className="text-sm text-gray-400">
+        code by Ipediez
       </Link>
       <SocialIcons />
     </footer>
